69f6892a460df4984d0afde5d27fae3099e09e69
unknown <sv73107535@idat.edu.pe>
2021-09-25 12:36:00 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./index.css";
+
+const Loggin = () => {
+    return (
+        <div className="loggin">
+            <div>Username</div>
+            <textarea></textarea>
+            <div>Password</div>
+            <textarea></textarea>
+        </div> 
+    )
+}
+
+export default Loggin;
